cbc01dfb2846d0d177e1f9f71fb04d772f4782e3
RenatoFringuello <renatofringuello@hotmail.it>
2023-10-01 16:05:43 +0200

    add answer slot add buttons, change photo style and responsiveness, add question props

--- src/components/partials/AppQuestion.vue
+++ src/components/partials/AppQuestion.vue
@@ -1,16 +1,30 @@
 <template>
     <section class="container d-flex">
-        <div class="wrapper m-auto">
+        <div class="wrapper py-5 m-auto">
 
-            <div class="row h-100">
-                <div class="col-12" :class="isPhotoMissing ?'' :'col-lg-6'">
-                    <!-- question -->
-                    <div class="my-title">{{ title }}</div>
-                    <slot name="simpleQuest"></slot>
+            <div class="row g-3 g-lg-4 h-100">
+                <div class="d-flex flex-column align-items-center justify-content-between col-12" :class="isPhotoMissing ?'' :'col-lg-8'">
+                    <div class="p-lg-5 ps-lg-0">
+                        <!-- question -->
+                        <div class="top mb-5">
+                            <div class="my-sub-title mb-2">{{ title }}</div>
+                            <div class="question text-center">{{ question }}</div>
+                        </div>
+                        <slot name="answer"></slot>
+                        <div class="mt-5 btn-wrapper d-flex justify-content-between w-100">
+                            <!-- vedi tu quale usare per il carosello -->
+                            <a href="#" class="button">Prev</a>
+                            <button>Next</button>
+                        </div>
+                    </div>
                 </div>
-                <div v-if="!isPhotoMissing" class="bg-comp-accent col-12 col-lg-6">
-                    <!-- photo -->
-                    <img class="image-quest" :src="store.getImageSrc(photoSrc)" :alt="`${title}'s image'`">
+                <div v-if="!isPhotoMissing" class="col-12 col-lg-4 d-none d-lg-block">
+                    <div class="bg-comp-accent d-flex h-100 p-2">
+                        
+                        <!-- photo -->
+                        <img class="image-quest m-auto" :src="store.getImageSrc(photoSrc)" :alt="`${title}'s image'`">
+                        
+                    </div>
                 </div>
             </div>
 
@@ -26,6 +40,10 @@ export default {
         title:{
             type : String,
             default : ''
+        },
+        question:{
+            type : String,
+            default : 'no Question'
         },
         photoSrc:{
             type : String,
@@ -52,10 +70,15 @@ export default {
         .wrapper{
             width: 100%;
 
+            .question{
+                font-size: 2.3rem;
+                line-height: 1;
+            }
             img.image-quest{
+                max-width: 400px;
                 width: 100%;
                 height: 100%;
-                object-fit: contain;
+                object-fit: cover;
                 object-position: top;
             }
         }
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,9 @@
 <template>
-    <AppQuestion title="Hello">
-        <template v-slot:simpleQuest>
+    <!-- comunicazione diretta = "dai ci siamo quasi; Ciao, come va?; ora rispondiamo a poche domande; ..." -->
+    <AppQuestion title="Categoria Domanda / comunicazione diretta" question="Come desideri che sia il tuo giorno più importante?" photoSrc="bride-1.jpg">
+        <template v-slot:answer>
+            <!-- <input type="text" name="answ" placeholder="... scrivici pure una risposta breve"> -->
+            <textarea rows="5" name="answ" placeholder="... scrivici pure una risposta breve"></textarea>
         </template>
     </AppQuestion>
 </template>
