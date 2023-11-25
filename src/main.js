0ad8d5576a4e16b13943d4c01b8c9d292a51493e
RenatoFringuello <renatofringuello@hotmail.it>
2023-11-25 18:33:41 +0100

    add formkit to main

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
+import config from './formkit.config.js'
+import { plugin, defaultConfig } from '@formkit/vue'
 import { router } from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(plugin, defaultConfig(config))
+    .mount('#app')
